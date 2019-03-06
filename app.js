c601a5a2b32e8a9afe4487d0b597144920d1b657
xuyan <xuyan@learnta.com>
2019-03-07 01:00:54 +0800

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const requset = require('superagent')
 const app = express()
 
 // parse application/x-www-form-urlencoded
@@ -18,7 +19,17 @@ app.post('/dingding', async (req, res) => {
   const data = req.body
   const { username } = data.actor;
   const { fullName, links } = data.repository;
-  console.log(links, data.push)
+  request
+    .post('https://oapi.dingtalk.com/robot/send?access_token=b57e5b4ed545bb728fc4c4412a8c669eee17e5b3b2a164653cbf1b9653d5e4f1')
+    .send({
+        "msgtype": "markdown",
+        "markdown": {"title":"杭州天气", "text":"[天气](http://www.thinkpage.cn/) ",}
+    })
+    .set('Accept', 'application/json')
+    .then(res => {
+        console.log(res)
+    });
+  
 
 })
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
