1ae14acdba1ed0a2f3b57326b85d390f220d999d
xuyan <xuyan@learnta.com>
2019-03-11 10:28:36 +0800

    test

--- app.js
+++ app.js
@@ -19,17 +19,36 @@ app.post('/dingding', async (req, res) => {
   const data = req.body
   const { username } = data.actor
   const { fullName, links } = data.repository
-  request
+  await request
     .post('https://oapi.dingtalk.com/robot/send?access_token=b57e5b4ed545bb728fc4c4412a8c669eee17e5b3b2a164653cbf1b9653d5e4f1')
     .send({
         "msgtype": "markdown",
-        "markdown": {"title":"杭州天气", "text":"[天气](http://www.thinkpage.cn/) ",}
+        "markdown": {
+            "title":fullName,
+            "text": `${username} 推送新变动到 xxx`
+        }
     })
     .set('Accept', 'application/json')
     .then(res => {
-        console.log(res)
+        
     });
   
 
 })
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// push
+// { 
+//     actor: { username: 'xuyan', displayName: 'xuyan' },
+//     repository: { 
+//         scmId: 'git',
+//         project: { key: 'TES', name: 'test_dingding' },
+//         slug: 'dingdingtalk',
+//         links: { self: [Array] },
+//         fullName: 'TES/dingdingtalk',
+//         ownerName: 'TES',
+//         public: false,
+//         owner: { username: 'TES', displayName: 'TES' } 
+//     },
+//     push: { changes: [ [Object] ] } 
+// }
